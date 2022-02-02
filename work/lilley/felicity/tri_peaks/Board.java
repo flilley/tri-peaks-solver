2ae789da0138dfcc86694588bc23912d52e22e82
Felicity Lilley <lilley.felicity@gmail.com>
2022-02-01 21:14:28 -0500

    Move stock to own class

--- work/lilley/felicity/tri_peaks/Board.java
+++ work/lilley/felicity/tri_peaks/Board.java
@@ -1,57 +1,21 @@
 package work.lilley.felicity.tri_peaks;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 class Board {
-  private static final int EXPECTED_STOCK_SIZE = 24;
-  private final List<Card> stock;
-  private final List<Card> waste;
+  private final Stock stock;
 
-  private Board(List<Card> stock) {
+  private Board(Stock stock) {
     this.stock = stock;
-    this.waste = new LinkedList<>();
   }
 
-  public static Board from(String stock) {
-    List<Card> stockCards = Card.from(stock);
-
-    if (stockCards.size() != EXPECTED_STOCK_SIZE) {
-      throw new IllegalArgumentException(String.format("Expected %d stock cards but got %d", EXPECTED_STOCK_SIZE, stockCards.size()));
-    }
-
-    return new Board(stockCards);
-  }
-
-  public Card discard() {
-    this.waste.add(0, this.stock.remove(0));
-    return this.waste.get(0);
-  }
-
-  public Card undoDiscard() {
-    this.stock.add(0, this.waste.remove(0));
-    return this.waste.get(0);
+  public static Board from(String stockInput) {
+    return new Board(Stock.from(stockInput));
   }
 
   public void reset() {
-    while(!waste.isEmpty()) {
-      this.stock.add(0, this.waste.remove(0));
-    }
+    this.stock.reset();
   }
 
   public void print(boolean showHidden) {
-    String cardString = showHidden ? getFullStockString() : getSummaryStockString();
-    System.out.println(String.format("The cards in the stock are: %s", cardString));
-  }
-
-  private String getFullStockString() {
-    return stock.stream()
-    .map(c -> c.getValue().toString())
-    .collect(Collectors.joining("->"));
-  }
-
-  private String getSummaryStockString() {
-    return String.format("%s (%d)", stock.get(0).getValue(), stock.size() - 1);
+    System.out.println(String.format("The cards in the stock are: %s", stock.getDisplayValue(showHidden)));
   }
 }
---
+++ work/lilley/felicity/tri_peaks/Stock.java
@@ -0,0 +1,72 @@
+package work.lilley.felicity.tri_peaks;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.AbstractMap.SimpleImmutableEntry;
+import java.util.stream.Collectors;
+
+class Stock {
+  private static final int EXPECTED_STOCK_SIZE = 24;
+
+  private final List<Card> stock;
+  // Boolean represents whether the card was added from the stock (true) or the peaks (false)
+  private final List<SimpleImmutableEntry<Card, Boolean>> waste;
+
+  private Stock(List<Card> stock) {
+    this.stock = stock;
+    this.waste = new LinkedList<SimpleImmutableEntry<Card, Boolean>>();
+  }
+
+  public static Stock from(String stockInput) {
+    List<Card> stockCards = Card.from(stockInput);
+
+    if (stockCards.size() != EXPECTED_STOCK_SIZE) {
+      throw new IllegalArgumentException(String.format("Expected %d stock cards but got %d", EXPECTED_STOCK_SIZE, stockCards.size()));
+    }
+
+    return new Stock(stockCards);
+  }
+
+  public Card discard() {
+    return this.discard(stock.remove(0), true);
+  }
+
+  public Card discard(Card cardToDiscard) {
+    return this.discard(cardToDiscard, false);
+  }
+
+  private Card discard(Card cardToDiscard, Boolean fromStock) {
+    this.waste.add(0, new SimpleImmutableEntry<>(cardToDiscard, fromStock));
+    return cardToDiscard;
+  }
+
+  public void undo() {
+    SimpleImmutableEntry<Card, Boolean> lastDiscarded = this.waste.remove(0);
+    if(lastDiscarded.getValue()) {
+      this.stock.add(0, lastDiscarded.getKey());
+    }
+  }
+
+  public void reset() {
+    while(!waste.isEmpty()) {
+      SimpleImmutableEntry<Card, Boolean> wasteTop = this.waste.remove(0);
+      if (wasteTop.getValue()) {
+        this.stock.add(0, wasteTop.getKey());
+      }
+    }
+  }
+
+  public String getDisplayValue(boolean showHidden) {
+    return showHidden ? getFullStockString() : getSummaryStockString();
+  }
+
+  private String getFullStockString() {
+    return stock.stream()
+    .map(c -> c.getValue().toString())
+    .collect(Collectors.joining("->"));
+  }
+
+  private String getSummaryStockString() {
+    return String.format("%s (%d)", stock.get(0).getValue(), stock.size() - 1);
+  }
+}
