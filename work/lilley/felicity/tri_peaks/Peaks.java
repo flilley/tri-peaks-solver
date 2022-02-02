3adf347953fec068dc3d9cebb4b8ca71966c1ac6
Felicity Lilley <lilley.felicity@gmail.com>
2022-02-01 23:50:48 -0500

    Add blocking positions

--- work/lilley/felicity/tri_peaks/Peaks.java
+++ work/lilley/felicity/tri_peaks/Peaks.java
@@ -1,6 +1,7 @@
 package work.lilley.felicity.tri_peaks;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.HashSet;
@@ -76,19 +77,44 @@ public class Peaks {
   }
 
   private static final class Position {
-    final int level;
-    final int slot;
+    private static final Map<Position, Set<Position>> BLOCKED_POSITIONS;
+    private final int level;
+    private final int slot;
+
+    static {
+      Map<Position, Set<Position>> blockedPositions = new HashMap<>();
+
+      for(int i = 0; i < SLOT_COUNT_BY_LEVEL.get(0); i++ ) {
+        blockedPositions.put(new Position(0, i), Set.of(new Position(1, 2*i), new Position(1, 2*i + 1)));
+      }
+
+      int first = 0;
+      for(int i = 0; i < SLOT_COUNT_BY_LEVEL.get(1); i++ ) {
+        blockedPositions.put(new Position(1, i), Set.of(new Position(2, first), new Position(2, first + 1)));
+        first += i % 2 == 1?  2 : 1;
+      }
+
+      for(int i = 0; i < SLOT_COUNT_BY_LEVEL.get(2); i++ ) {
+        blockedPositions.put(new Position(2, i), Set.of(new Position(3, i), new Position(3, i + 1)));
+      }
+
+      BLOCKED_POSITIONS = Collections.unmodifiableMap(blockedPositions);
+    }
 
     private Position(int level, int slot) {
       this.level = level;
       this.slot = slot;
     }
 
-    static Position from(int level, int slot) {
+    protected static Position from(int level, int slot) {
       if (level > MAX_LEVEL || slot >= SLOT_COUNT_BY_LEVEL.get(level)) {
         throw new RuntimeException(String.format("(%d, %s) is not a valid position, level, slot"));
       }
       return new Position(level, slot);
+    }
+
+    protected Set<Position> getPositionsToUnblock() {
+      return BLOCKED_POSITIONS.get(this);
     }
 
     @Override
